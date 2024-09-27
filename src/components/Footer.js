9ecabb032a6f2839faa2a49a9fa195f3a92f47aa
Talha Khalil <93043845+Talhakhalil01@users.noreply.github.com>
2024-09-27 23:01:46 +0500

    Updated Social Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,10 +12,10 @@ function Footer() {
           <p>© <span>Copyright</span> <strong className="px-1 sitename">DevMERN</strong> <span>All Rights Reserved</span></p>
         </div>
         <div className={`${styles['social-links']} d-flex justify-content-center`}>
-          <a href=""><i className="bi bi-twitter-x"></i></a>
-          <a href=""><i className="bi bi-facebook"></i></a>
-          <a href=""><i className="bi bi-instagram"></i></a>
-          <a href=""><i className="bi bi-linkedin"></i></a>
+               <a href="https://www.facebook.com/share/sRoQaog92N8CyCPq/?mibextid=qi2Omg" target="_blank"><i className="bi bi-facebook"></i></a>
+               <a href="https://www.instagram.com/talhakhalil68?igsh=eDNsbm9wMmE3cmt2" target="_blank"><i className="bi bi-instagram"></i></a>
+               <a href="https://www.linkedin.com/in/talha-khalil-9415b02a4?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"><i className="bi bi-linkedin"></i></a>
+     
         </div>
         <div className={styles.credits}>
           Designed with &#10084; by <a href="https://bootstrapmade.com/">Talha Khalil</a>
